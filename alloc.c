ffe2dee9d8757ffbdf8fb7aa2cbbf4d51097e299
Ubuntu <azureuser@LYCAzure.wdu32gdrqhwejp0mdkcjbchilb.syx.internal.cloudapp.net>
2023-02-06 01:09:35 +0000

    LYC - 86.6

--- alloc.c
+++ alloc.c
@@ -21,21 +21,135 @@ typedef struct {
 void* heap_begin = NULL;
 void* heap_end = NULL;
 
-link_node freeList[24] = {0, };
+link_node freeList[32] = {0, };
 
 uint32_t inline PtrToIdx(void* ptr){
     //주요 최적화 포인트
-    for(int i = 2; i >= 0 ; i--){
-        char c = *((char*)ptr + i);
-        if(c != 0){
-            for(int j = 7; j >= 0; j--){
-                if(c & 0x80){
-                    return j + (i*8);
-                }
-                c = c << 1;
+    //이진조건
+    int value = *(int*)ptr;
+    if(value & 0xFFFF0000){
+        //상위 2바이트에 값이 있을 경우
+        if(value & 0xFF000000){
+            //4번째 바이트에 값이 있을 경우
+            if(value & 0xF0000000){
+                //상위 4비트에 값이 있다면 (0xF0000000)
+                if(value & 0x80000000)
+                    return 31;
+                if(value & 0x40000000)
+                    return 30;
+                if(value & 0x20000000)
+                    return 29;
+                if(value & 0x10000000)
+                    return 28;
+            }
+            else{
+                //하위 4비트에 값이 있다면 (0x0F000000)
+                if(value & 0x08000000)
+                    return 27;
+                if(value & 0x04000000)
+                    return 26;
+                if(value & 0x02000000)
+                    return 25;
+                if(value & 0x01000000)
+                    return 24;
+            }
+        }
+        else{
+            //3번째 바이트에 값이 있을 경우
+            if(value & 0x00F00000){
+                //상위 4비트에 값이 있다면 (0x00F00000)
+                if(value & 0x00800000)
+                    return 23;
+                if(value & 0x00400000)
+                    return 22;
+                if(value & 0x00200000)
+                    return 21;
+                if(value & 0x00100000)
+                    return 20;
+
+            }
+            else{
+                //하위 4비트에 값이 있다면 (0x000F0000)
+                if(value & 0x00080000)
+                    return 19;
+                if(value & 0x00040000)
+                    return 18;
+                if(value & 0x00020000)
+                    return 17;
+                if(value & 0x00010000)
+                    return 16;
             }
         }
     }
+    else{
+        //하위 2바이트만 확인
+        if(value & 0x0000FF00){
+            //2번째 바이트에 값이 있을 경우
+            if(value & 0xF000){
+                //상위 4비트에 값이 있다면(0x0000F000)
+                if(value & 0x00008000)
+                    return 15;
+                if(value & 0x00004000)
+                    return 14;
+                if(value & 0x00002000)
+                    return 13;
+                if(value & 0x00001000)
+                    return 12;
+            }
+            else{
+                //하위 4비트에 값이 있다면(0x00000F00)
+                if(value & 0x00000800)
+                    return 11;
+                if(value & 0x00000400)
+                    return 10;
+                if(value & 0x00000200)
+                    return 9;
+                if(value & 0x00000100)
+                    return 8;
+            }
+        }
+        else{
+            //1번째 바이트에 값이 있을 경우
+            if(value & 0xF0){
+                //상위 4비트에 값이 있다면(0x000000F0)
+                if(value & 0x00000080)
+                    return 7;
+                if(value & 0x00000040)
+                    return 6;
+                if(value & 0x00000020)
+                    return 5;
+                if(value & 0x00000010)
+                    return 4;
+            }
+            else{
+                //하위 4비트에 값이 있다면(0x0000000F)
+                if(value & 0x00000008)
+                    return 3;
+                if(value & 0x00000004)
+                    return 2;
+                if(value & 0x00000002)
+                    return 1;
+                if(value & 0x00000001)
+                    return 0;
+            }
+
+        }
+
+    }
+
+
+
+    // for(int i = 2; i >= 0 ; i--){
+    //     char c = *((char*)ptr + i);
+    //     if(c != 0){
+    //         for(int j = 7; j >= 0; j--){
+    //             if(c & 0x80){
+    //                 return j + (i*8);
+    //             }
+    //             c = c << 1;
+    //         }
+    //     }
+    // }
 }
 
 void pop(void* ptr, uint32_t idx){
@@ -128,7 +242,7 @@ void* GetMemory(uint32_t volSize){
     char* resultPtr = NULL;
     uint32_t idx = PtrToIdx(&volSize);
     link_node* tmp = NULL;
-    for(; idx < 24; idx++){
+    for(; idx < 32; idx++){
         resultPtr = freeList[idx].next;
         while(resultPtr != NULL){
             tmp = (link_node*) resultPtr;
@@ -149,7 +263,7 @@ void* GetMemory(uint32_t volSize){
         }
         resultPtr = sbrk(volSize);
         heap_end += volSize;
-        volSize++;
+        volSize++;      //VolSize = Volsize | 1
         memcpy(resultPtr, &volSize, 4);
         memcpy(heap_end - 4, &volSize, 4);
         return resultPtr + 4;
